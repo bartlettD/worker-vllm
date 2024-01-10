87d5fc24cdb9bba18cc1511054a3cbfcc5d0ae34
Dylan Bartlett <dylanbrtlett@gmail.com>
2024-01-10 09:16:54 +0000

    Try adding trust_remote_code to engine config

--- src/engine.py
+++ src/engine.py
@@ -1,4 +1,4 @@
-import os
+eimport os
 import logging
 from typing import Union
 import torch
@@ -47,6 +47,7 @@ class VLLMEngine:
             "disable_log_requests": bool(int(os.getenv("DISABLE_LOG_REQUESTS", 1))),
             "gpu_memory_utilization": float(os.getenv("GPU_MEMORY_UTILIZATION", 0.98)),
             "tensor_parallel_size": self._get_num_gpu_shard(),
+            "trust_remote_code": True,
         }
 
     def _initialize_llm(self):
